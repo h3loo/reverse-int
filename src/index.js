c6ca1db681dd8f7453ce27c17ce2dd5d8e3e599e
becker <dmitryymurov@gmail.com>
2023-01-25 16:18:51 +0200

    feat: reverse number task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    
+    const normalNum = Math.abs(n);
+    const splitNum = normalNum.toString().split("");
+    const reversArray = splitNum.reverse();
+    const joinArray = reversArray.join("");
+    
+    return joinArray;
 }
